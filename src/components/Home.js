4cb7915d820581e9ce9a0750eac572f32416d325
Darien Salazar <darien.axell.salazar@gmail.com>
2022-10-06 11:29:06 -0500

    Created useFetch component:

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,8 @@
-import {useState, useEffect} from "react";
+import useFetch from "../useFetch";
 import BlogList from './BlogList'
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch(" http://localhost:8000/blogs")
-            .then(res => {
-                if (!res.ok) {
-                    throw Error('Oh  no! Something is wrong...')
-                }
-               return res.json()
-            })
-            .then(data => {
-                console.log(data)
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err => {
-                setError(err.message)
-                setIsPending(false);
-            })
-    }, []);
+    const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
 
     return (
         <div className="home">
